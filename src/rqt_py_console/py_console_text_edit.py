01be0e251ba474344ab2ebe3d101f2c5f248da2d
Chris Lalancette <clalancette@gmail.com>
2025-01-03 04:57:49 -0500

    Add the standard tests to rqt_py_console. (#19)
    That way we know it conforms to our standards.
    
    Signed-off-by: Chris Lalancette <clalancette@gmail.com>

--- src/rqt_py_console/py_console_text_edit.py
+++ src/rqt_py_console/py_console_text_edit.py
@@ -1,5 +1,3 @@
-# Software License Agreement (BSD License)
-#
 # Copyright (c) 2012, Dorian Scholz
 # All rights reserved.
 #
@@ -7,21 +5,21 @@
 # modification, are permitted provided that the following conditions
 # are met:
 #
-#  * Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-#  * Redistributions in binary form must reproduce the above
-#    copyright notice, this list of conditions and the following
-#    disclaimer in the documentation and/or other materials provided
-#    with the distribution.
-#  * Neither the name of Willow Garage, Inc. nor the names of its
-#    contributors may be used to endorse or promote products derived
-#    from this software without specific prior written permission.
+#   * Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+#   * Redistributions in binary form must reproduce the above
+#     copyright notice, this list of conditions and the following
+#     disclaimer in the documentation and/or other materials provided
+#     with the distribution.
+#   * Neither the name of the Willow Garage, Inc. nor the names of its
+#     contributors may be used to endorse or promote products derived
+#     from this software without specific prior written permission.
 #
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 # "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 # LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
 # FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
 # INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
 # BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
 # LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
@@ -30,8 +28,8 @@
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-import sys
 from code import InteractiveInterpreter
+import sys
 
 from python_qt_binding import QT_BINDING, QT_BINDING_VERSION
 from python_qt_binding.QtCore import Qt, Signal
@@ -44,7 +42,7 @@ class PyConsoleTextEdit(ConsoleTextEdit):
     _multi_line_char = ':'
     _multi_line_indent = '    '
     _prompt = ('>>> ', '... ')  # prompt for single and multi line
-    exit = Signal()
+    widget_exit = Signal()
 
     def __init__(self, parent=None):
         super(PyConsoleTextEdit, self).__init__(parent)
@@ -66,4 +64,4 @@ class PyConsoleTextEdit(ConsoleTextEdit):
         try:
             self._interpreter.runsource(code)
         except SystemExit:  # catch sys.exit() calls, so they don't close the whole gui
-            self.exit.emit()
+            self.widget_exit.emit()
